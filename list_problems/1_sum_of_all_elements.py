9d55af843e6c8d3fd285aa0fe09e1b8356d47b1f
vinit <vinitkkulkarni@gmail.com>
2023-10-13 18:13:40 +0530

    added:program to caluculate sum of all the elements in list

---
+++ list_problems/1_sum_of_all_elements.py
@@ -0,0 +1,15 @@
+'''
+@Author: Vinit 
+@Date: 2023-10-13 10:00:00
+@Last Modified by: None
+@Last Modified time: None
+@Title : Write a Python program to sum all the items in a list
+'''
+
+my_list = [1,2,3,4,5]
+sum_of_elements = 0
+
+for item in my_list:
+    sum_of_elements += item 
+
+print("sum of elements: ",sum_of_elements)
