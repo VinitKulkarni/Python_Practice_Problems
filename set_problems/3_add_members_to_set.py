e04af780190de906f3778fda66666d9f2d8a7be0
vinit <vinitkkulkarni@gmail.com>
2023-10-13 17:59:48 +0530

    added:program to add the members to set

---
+++ set_problems/3_add_members_to_set.py
@@ -0,0 +1,16 @@
+'''
+@Author: Vinit 
+@Date: 2023-10-13 13:45:00
+@Last Modified by: None
+@Last Modified time: None
+@Title : Write a Python program to add member(s) in a set.
+'''
+
+my_set = {1,2,"hi"}
+print(my_set)
+
+my_set.add("there")
+print(my_set)
+
+my_set.update(["how","are",'You'])
+print(my_set)
