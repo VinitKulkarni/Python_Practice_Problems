8f9bebc9528bfded3fe5b51c8f874989627e17b6
vinit <vinitkkulkarni@gmail.com>
2023-10-13 18:22:39 +0530

    added:append one list to another

---
+++ list_problems/13_append_the_list.py
@@ -0,0 +1,15 @@
+'''
+@Author: Vinit 
+@Date: 2023-10-13 10:00:00
+@Last Modified by: None
+@Last Modified time: None
+@Title : Write a Python program to append a list to the second list.
+'''
+
+
+list_one = ["sachine","kohli","rohit"]
+list_two = ["shami","ashwin"]
+
+list_one.append(list_two)
+
+print(list_one)
