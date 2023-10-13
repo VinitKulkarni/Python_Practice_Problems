3931e1ce187df1faa1c905cea707e001e5288371
vinit <vinitkkulkarni@gmail.com>
2023-10-13 17:58:02 +0530

    added:program to create the set

---
+++ set_problems/1_create_set.py
@@ -0,0 +1,15 @@
+'''
+@Author: Vinit 
+@Date: 2023-10-13 13:02:00
+@Last Modified by: None
+@Last Modified time: None
+@Title : Write a Python program to create a set.
+'''
+
+my_set1 = {1,2,'three','4',"five"}
+print(my_set1)
+print(type(my_set1))
+
+my_set2= {1,2,'3',3}
+print(my_set2)
+print(type(my_set2))
