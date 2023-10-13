4197d8d88b2e9ae373c4722108cd0b921982ff2b
vinit <vinitkkulkarni@gmail.com>
2023-10-13 18:07:49 +0530

    added:program to perform symmetric difference operation on sets

---
+++ set_problems/9_symmetric_diff.py
@@ -0,0 +1,16 @@
+'''
+@Author: Vinit 
+@Date: 2023-10-13 14:10:00
+@Last Modified by: None
+@Last Modified time: None
+@Title : Write a Python program to create a symmetric difference 
+'''
+
+
+sample_set1 = {1,2,3}
+sample_set2 = {3,4,5}
+
+print(sample_set1.symmetric_difference(sample_set2))
+
+
+#print(sample_set1 ^ sample_set2)
