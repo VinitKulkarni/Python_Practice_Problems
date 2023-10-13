9dd7eef7049ad1a094b0191ddfec4d9ae089438a
vinit <vinitkkulkarni@gmail.com>
2023-10-13 18:18:23 +0530

    added:program for copy the list

---
+++ list_problems/7_copy_list.py
@@ -0,0 +1,14 @@
+'''
+@Author: Vinit 
+@Date: 2023-10-13 10:00:00
+@Last Modified by: None
+@Last Modified time: None
+@Title : Write a Python program to clone or copy a list.
+'''
+
+
+this_list = [1,2,3,4,5]
+
+my_list = this_list.copy()
+
+print(my_list)
