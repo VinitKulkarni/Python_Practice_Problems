a9ca1604cef56084de59aff0e6f064778da7b2d7
vinit <vinitkkulkarni@gmail.com>
2023-10-14 10:22:52 +0530

    added:program to check the element exists in tuple or not

---
+++ tuple_problems/6_element_exits_in_tuple.py
@@ -0,0 +1,14 @@
+'''
+@Author: Vinit 
+@Date: 2023-10-13 15:00:00
+@Last Modified by: None
+@Last Modified time: None
+@Title : Write a Python program to check whether an element exists within a tuple
+'''
+
+
+my_tuple = (1, 2, 3.3, 'four', True, [])
+
+
+print(3.3 in my_tuple)
+print('3.3' in my_tuple)
