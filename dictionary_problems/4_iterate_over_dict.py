14b83ff0c0467dd2c19b5bec776e8c12b8334e4c
vinit <vinitkkulkarni@gmail.com>
2023-10-14 19:27:03 +0530

    added:program to iterate over the dictionary

---
+++ dictionary_problems/4_iterate_over_dict.py
@@ -0,0 +1,12 @@
+'''
+@Author: Vinit 
+@Date: 2023-10-14 15:00:00
+@Last Modified by: None
+@Last Modified time: None
+@Title : Write a Python program to iterate over dictionaries using for loops.
+'''
+
+sample_dict = {'fname': "Sachin", "lname": "Tendulkar", 'age': 50}
+
+for key, value in sample_dict.items():
+    print(key," ",value)
