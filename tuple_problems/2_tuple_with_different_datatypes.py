2024aa1b0624dcb3f0940c20c8bcf853bc1b579b
vinit <vinitkkulkarni@gmail.com>
2023-10-14 10:17:12 +0530

    added:program to create tuple with different datatypes

---
+++ tuple_problems/2_tuple_with_different_datatypes.py
@@ -0,0 +1,11 @@
+'''
+@Author: Vinit 
+@Date: 2023-10-13 14:43:00
+@Last Modified by: None
+@Last Modified time: None
+@Title : Write a Python program to create a tuple with different data types.
+'''
+
+my_tuple = (1,2,3.3,'four',True)
+print(type(my_tuple))
+print(my_tuple)
