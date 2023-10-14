3f85c03f68707c86e29fb24de4f5753eec38653c
vinit <vinitkkulkarni@gmail.com>
2023-10-14 10:25:47 +0530

    added:program to reverse the tuple

---
+++ tuple_problems/10_reverse_tuple.py
@@ -0,0 +1,11 @@
+'''
+@Author: Vinit 
+@Date: 2023-10-13 16:23:00
+@Last Modified by: None
+@Last Modified time: None
+@Title : Write a Python program to reverse a tuple.
+'''
+
+my_tuple = (1, 2, 3.3, 'four', True, [])
+print("original = ", my_tuple)
+print("reversed = ",my_tuple[::-1])
